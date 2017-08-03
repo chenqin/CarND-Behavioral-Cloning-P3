bb2aa49781b80923f3a453be5a37264a734f21c7
Chen Qin <qinnchen@fgmail.com>
2017-08-03 00:06:00 -0700

    starts implement nvida convnet

--- model.py
+++ model.py
@@ -3,24 +3,26 @@ from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
 import ipdb
+import random
 from keras.backend import tf as ktf
 import math
 import cv2
 import os
+
+from keras.layers import Dense, ZeroPadding2D, Convolution2D, MaxPooling2D, Dropout
+from keras.layers import Flatten
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers.normalization import BatchNormalization
 
 def my_resize_function(input):
     return ktf.image.resize_images(input, (64, 64))
 
 
 def load_image(filepath):
-	path = '/Users/chenqin/CarND-Behavioral-Cloning-P3/data/IMG/' + os.path.split(filepath)[1]
-	return cv2.imread(path, 0)
+    path = '/Users/chenqin/CarND-Behavioral-Cloning-P3/data/IMG/' + os.path.split(filepath)[1]
+    return cv2.imread(path)
 
-# open as grayscale, crop non road portion of image, original image is 160x320
-def crop_non_road(filepath):
-    img = cv2.imread(filepath, 0)
-    crop_img = img[50:130,:]
-    return crop_img
 
 # preprocessing image files
 # flip image, and change steering to opposite direction
@@ -31,40 +33,58 @@ def X_train_gen(trainning):
     lefts = trainning['left'].values
     rights = trainning['right'].values
     steerings = trannings['steering'].values
-    x = np.zeros((64, 64, 3))
-    X_train = np.zeros(len(steerings), 64, 64, 3)
+
+    X_train = np.zeros((len(steerings), 66, 200, 3))
     y_train = np.zeros(len(steerings))
+
     for i in range(1, len(centers)):
         #overlap left , center, right into trainning set
-        center = load_image(centers[i])[50:130, :]
-        left = load_image(lefts[i])[50:130, :]
-        right = load_image(rights[i])[50:130, :]
-        x = (i, my_resize_function(left), my_resize_function(center), my_resize_function(right))
-        y = float(steerings[i])
-        X_train[i] = x
-        y_train[i] = y
+        choice = randint(0,2)
+        filepath = ""
+        if choice == 0:
+            filepath = centers[i]
+            y_train[i] = float(steerings[i])
+        elif choice == 1:
+            filepath = lefts[i]
+            y_train[i] = float(steerings[i]) + 0.3
+        else:
+            filepath = rights[i]
+            y_train[i] = float(steerings[i]) - 0.3
+
+        image = load_image(filepath)[50:130, :]
+        #convert to YUV planes
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+        #resize image to
+        image = cv2.resize(image, (66, 200), interpolation=cv2.INTER_AREA)
+
+        # do random flip of 50% of images to avoid left turn bias
+        if randint(0,1) == 1:
+            image = cv2.flip(image, 1)
+            y_train[i] = -y_train[i]
+
+        X_train[i] = image
     return X_train, y_train
 
 # load data
 trannings = pandas.read_csv('/Users/chenqin/CarND-Behavioral-Cloning-P3/data/driving_log.csv', names=['center', 'left', 'right', 'steering', 'throttle', 'break', 'speed'])
 y_train_org = trannings['steering'].values
 
-# drop 6/7 of straight moving examples, skip header index = 0
-drop_rows = [i for i in range(1, len(y_train_org)) if math.fabs(float(y_train_org[i])) < 0.25 and randint(0, 6) != 0]
+# drop 3/4 of straight moving examples, skip header index = 0
+drop_rows = [i for i in range(1, len(y_train_org)) if math.fabs(float(y_train_org[i])) < 0.25 and randint(0, 3) != 0]
 trannings.drop(drop_rows, inplace=True)
 
-
-
-# make sure steering data has nice distribution
-#plt.hist(pandas.to_numeric(trannings['steering'][1:]))
+# exame distribution of steering bias
+#plt.hist(y_train)
 #plt.ylabel('steering values distribution')
 #plt.show()
 
-X_train, y_train = X_train_gen(trainning=trannings)
-
-#test crop
-#cv2.imshow("cropped", crop_non_road('/Users/chenqin/CarND-Behavioral-Cloning-P3/data/IMG/center_2017_07_30_13_22_07_801.jpg'))
-#cv2.waitKey(0)
-
 #TODO: transfer learning with nvidia model
 
+def nvida_model(X_train, y_train):
+    model = Sequential()
+    model.add(BatchNormalization())
+    model.add(Convolution2D())
+
+
+X_train, y_train = X_train_gen(trainning=trannings)
+nvida_model(X_train, y_train)
